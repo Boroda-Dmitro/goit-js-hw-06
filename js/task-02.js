f71da9ef06b46f854fc2ace64600b1a0a2cb13cc
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-01-25 19:52:12 +0200

    add 1-2 hw

---
+++ js/task-02.js
@@ -0,0 +1,22 @@
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+console.log(ingredientsEl);
+
+ingredients.forEach(item => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.classList = 'item';
+  ingredientItem.textContent = item;
+  ingredientsEl.append(ingredientItem);
+});
+
+
+const indigredientsElenents = ingredients.map(item => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.classList = 'item';
+  ingredientItem.textContent = item;
+  return ingredientItem;
+})
+
+ingredientsEl.append(...indigredientsElenents);
