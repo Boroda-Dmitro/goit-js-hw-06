b85461f8dfcfb0f0aa24b8d172f1eb2fa2020945
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-01-30 15:34:36 +0200

    add all to 9 hw

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+console.log(formEl);
+
+formEl.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  email.value === '' || password.value === ''
+    ? alert('You shall not pass!!! All fields must be filled')
+    : console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color');
+const widgetColor = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  widgetColor.textContent = getRandomHexColor();
+});
